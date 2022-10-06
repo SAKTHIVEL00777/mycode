c7b4c23d8b018239a0f4c89ed0f2ff60e65b0679
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-06 10:47:23 +0530

    my code

---
+++ First.java
@@ -0,0 +1,11 @@
+class First
+{
+public static void main(String arg[])
+{
+    System.out.println("WELCOME TO JAVA");  
+    System.out.println(" BATCH NO IS 26......");
+           
+}
+
+}
+
---
+++ Flipkart.java
@@ -0,0 +1,9 @@
+class Flipkart{
+
+public static void main(String arg[])
+
+{
+   System.out.println("product sales in online purches");
+}
+
+}

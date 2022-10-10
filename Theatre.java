669493dceb84ed36cf490f79d148fe4646005feb
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-10 10:31:02 +0530

    EB process success

---
+++ Theatre.java
@@ -0,0 +1,23 @@
+class Theatre{
+
+public static void main(String arg[])
+{
+     Theatre raja=new Theatre(); //Object Creation
+
+     int balance=raja.bookTicket(200);//Method Calling, receive and store data value of balance in balance
+         
+     System.out.println("After booking ticket"+ balance);//Concatination Operator ("String"+value)
+
+}
+
+int bookTicket(int ticket)
+{
+  int ticket_price=120;
+   
+  int balance= ticket-ticket_price;//return this value of balance
+  
+  return balance;
+  
+}
+
+}

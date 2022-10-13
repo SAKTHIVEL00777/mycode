3eaa048467430d783bfc8d9f93eb413f92aa0d68
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-13 14:05:23 +0530

    online purches

---
+++ Amazon.java
@@ -0,0 +1,17 @@
+class Amazon{
+
+ void purches()
+{
+    System.out.println("ONLINE PURCHES");
+
+}
+
+public static void main(String args[])
+ {
+
+       Amazon amazon=new Amazon();
+       amazon.purches();
+
+}
+
+}

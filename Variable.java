1a4c54ec5cca11d74b74520d7ce80079a9302645
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-17 16:52:55 +0530

    local instance static variable

---
+++ Variable.java
@@ -0,0 +1,31 @@
+class Variable{
+
+static float d=567.8f;
+
+int c=75;     //instance 
+
+void show(int a,int b){    //method definition
+    
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+   
+
+}
+
+public static void main(String args[]){
+
+   Variable var=new Variable();     //object created
+
+      var.show(57,39);             //method calling
+
+     // var.c=75;
+    System.out.println(var.c);   //instance method calling
+
+    System.out.println(d);      // static method calling
+
+
+}
+
+
+}

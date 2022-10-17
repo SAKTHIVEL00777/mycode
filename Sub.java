cfc54c54d578e8acda8b3bad4bc653b3bcd512f9
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-17 16:36:18 +0530

    local variable

---
+++ Sub.java
@@ -0,0 +1,23 @@
+class Sub{
+
+
+int bye(int a,int b){
+
+return a+b;
+
+}
+
+public static void main(String arg[]){
+
+System.out.println("hi..");
+
+Sub obj=new Sub();
+
+
+int yy=obj.bye(10,20);
+System.out.println(yy);
+
+}
+
+
+}

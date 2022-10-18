76b47dc4927f96ea18c60637640f49df294d0ddd
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-18 17:08:12 +0530

    parent to child

---
+++ Mobile.java
@@ -0,0 +1,35 @@
+class Mobile extends Telephone{
+
+int price=15000;
+
+String name="Mobile";
+
+  public void sms()
+    {
+    System.out.println("Messaging Facility"); 
+    }
+
+public static void main(String args[]){
+  
+   Mobile samsung=new Mobile();
+
+     samsung.sms();
+     samsung.call();
+     samsung.receive_call();
+     
+     
+     System.out.println(samsung.price);
+     System.out.println(samsung.name);
+
+   
+
+
+ 
+   Telephone bsnl=new Telephone();
+   
+      System.out.println(bsnl.price);
+      System.out.println(bsnl.name);
+
+}
+
+}

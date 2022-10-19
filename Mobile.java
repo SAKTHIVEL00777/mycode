edbd204eb1e2416786027bc0c34c73608066cd91
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-19 08:43:05 +0530

    parent to child

--- Mobile.java
+++ Mobile.java
@@ -13,22 +13,20 @@ public static void main(String args[]){
   
    Mobile samsung=new Mobile();
 
-     samsung.sms();
-     samsung.call();
-     samsung.receive_call();
-     
-     
-     System.out.println(samsung.price);
-     System.out.println(samsung.name);
-
+     System.out.println("Mobile Price:"+samsung.price);
+     System.out.println("Name:"+samsung.name);
    
-
-
+      samsung.sms();
+      samsung.call();
+      samsung.receive_call();
  
    Telephone bsnl=new Telephone();
    
-      System.out.println(bsnl.price);
-      System.out.println(bsnl.name);
+      System.out.println("Telephone Price"+bsnl.price);
+      System.out.println("Name:"+bsnl.name);
+
+      bsnl.call();
+      bsnl .receive_call();
 
 }
 

fa8c2bbcade561c18cc9697f4275473542b0d224
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-10-29 07:38:35 +0530

    getter & setter

---
+++ DiesalPunk.java
@@ -0,0 +1,25 @@
+class DiesalPunk{
+
+ float diesal=105.7f;
+ String name="raja";
+ int salary=12000;
+
+float getDiesal(){
+return this.diesal;
+}
+void setDiesal(float diesal){
+this.diesal=diesal;
+}
+public String getName(){
+return name;
+}
+public void steName(String name){
+this.name=name;
+}
+int getSalary(){
+return this.salary;
+}
+void setSalary(int salary){
+this.salary=salary;
+}
+}
---
+++ Lorry.java
@@ -0,0 +1,22 @@
+class Lorry{
+
+public static void main(String args[]){
+
+  DiesalPunk dp=new DiesalPunk();
+ 
+   System.out.println(dp.diesal);
+   System.out.println(dp.name);
+   System.out.println(dp.salary);
+
+   dp.diesal=107.4f;
+    System.out.println(dp.diesal);
+
+ // dp.setdiesal(103.7f);
+  
+
+
+
+}
+
+
+}

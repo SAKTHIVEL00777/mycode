fd79a69605dbec83b3554a8b477e742c7e28ea09
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-02 14:39:05 +0530

    Condition Statement

---
+++ ConditionLooping.java
@@ -0,0 +1,43 @@
+class ConditionLooping{
+  
+    void amount() 
+{
+        
+      int amount=0;
+      int day=1;
+      while(day<=10){
+          amount=amount+day;
+           day=day+1;
+       }
+         System.out.println("amount:" +amount);
+   } 
+    void dosai(int dosai)
+{
+
+      int count=3;
+      while(count>=1){
+              dosai=dosai+dosai/2;
+              count=count-1;
+       }
+       System.out.println("Dosai:" +dosai);
+}
+  void security(int beats)
+{
+       int security=0;
+       while(beats>1){
+        beats=beats/2;
+        security=security+1;
+      }
+        System.out.println("Security:" +security);
+}
+
+public static void main(String args[]){
+   
+     ConditionLooping loop=new ConditionLooping();
+      loop.amount();
+      loop.dosai(8);
+      loop.security(1024);
+
+}
+
+}

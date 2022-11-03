361fe0d7de543a0535fc368e610a59b80e2ca8df
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-03 18:04:34 +0530

    while loop

--- Hi.java
+++ Hi.java
@@ -7,13 +7,20 @@ class Hi{
   value=value+2;
 }
   System.out.println();
-
-
+}
+  void lineNumbers(){
+   int value=1;
+   while(value<=5){
+  System.out.print(value +" ");
+   value=value+1;
+}
+   System.out.println();
 }
 public static void main(String args[]){
 
    Hi hi=new Hi();
      hi.oddNumber();
+     hi.lineNumbers();
 }
 
 }

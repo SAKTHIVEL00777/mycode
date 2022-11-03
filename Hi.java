35191acbad930248d6900a601deca5facd6f07b2
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-03 09:44:42 +0530

    while

---
+++ Hi.java
@@ -0,0 +1,19 @@
+class Hi{
+
+ void oddNumber(){
+   int value=1;
+   while(value<=9){
+  System.out.print(value +" ");
+  value=value+2;
+}
+  System.out.println();
+
+
+}
+public static void main(String args[]){
+
+   Hi hi=new Hi();
+     hi.oddNumber();
+}
+
+}

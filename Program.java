9c33b0170fcbdb1fa8e80e0dcc23507f89b0f3bc
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-04 17:33:47 +0530

    While Loop Program

--- Program.java
+++ Program.java
@@ -1,5 +1,27 @@
 class Program{
 
+
+  void printOne() {
+					
+					int start=1;
+					int end=6;
+					while(start<end)
+                   {
+						System.out.print(1 + " ");
+						start=start+1;
+						}
+				   System.out.println();
+				}
+  void print12345() {
+					int start=1;
+					int end=6;
+					while(start<end)
+                  {
+						System.out.print(start + " ");
+						start=start+1;
+					}
+					System.out.println();
+				}
 //void multiple_of_3_and_5(){
  void multipleAnd(){
   int no =1;
@@ -26,20 +48,29 @@ class Program{
   }
        System.out.println();
  }
+	void sum_of_first_n_numbers() {
+				int sum=0;
+				int no=1;
+				while(no<=10) 
+             {
+					sum=sum+no;
+					no=no+1;
+				}
+				System.out.println(sum);
+			}
 
-
-
- 
- 
  public static void main(String args[]){
  
    Program pro=new Program();
 
      //pro.multiple_of_3_and_5();
      //pro. multiples_of_3_or_5();
-
+       
+       pro.printOne();
+	   pro.print12345();
        pro.multipleAnd();
        pro.multipleOR();
+       pro.sum_of_first_n_numbers();
      
   
 

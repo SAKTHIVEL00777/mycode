77a6eb8018e88dbce3fc68e9f687693a82aeb854
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-05 09:02:03 +0530

    While Loop Program

--- Program.java
+++ Program.java
@@ -23,31 +23,31 @@ class Program{
 					System.out.println();
 				}
 //void multiple_of_3_and_5(){
- void multipleAnd(){
-  int no =1;
-  while(no<=30)
-{
-   if(no%3==0 && no%5==0){
-     System.out.print(no+ " ");
-   }
-    no=no+1;
-} 
-   System.out.println();
+  void multipleAnd(){
+                    int no =1;
+                    while(no<=30)
+                  {
+                     if(no%3==0 && no%5==0){
+                       System.out.print(no+ " ");
+                      }
+                    no=no+1;
+                 } 
+                   System.out.println();
 
-}
+               }
  //void multiples_of_3_or_5() {
     void multipleOR(){
-  int no = 1; 
-  while(no<=30) // | - pipe
-  {
-  if(no%3==0 || no%5==0)
-   {
-   System.out.print(no+"  "); 
-   }
-  no = no+1; 
-  }
-       System.out.println();
- }
+                      int no = 1; 
+                      while(no<=30) // | - pipe
+                   {
+                      if(no%3==0 || no%5==0)
+                       {
+                          System.out.print(no+"  "); 
+                         }
+                         no = no+1; 
+                   }
+                      System.out.println();
+              }
 	void sum_of_first_n_numbers() {
 				int sum=0;
 				int no=1;
@@ -58,6 +58,19 @@ class Program{
 				}
 				System.out.println(sum);
 			}
+    void count_of_digits(int no) {
+		     int count=0;
+		     while(no>0) {
+		    	 System.out.print(no%10);
+		    	 count=count+1;
+		    	 no=no/10;
+		     }
+	 	     System.out.println(" No of Digits is:"+count);
+	}
+
+	
+	
+    
 
  public static void main(String args[]){
  
@@ -71,6 +84,10 @@ class Program{
        pro.multipleAnd();
        pro.multipleOR();
        pro.sum_of_first_n_numbers();
+       pro.count_of_digits(5674);
+       
+    
+
      
   
 
